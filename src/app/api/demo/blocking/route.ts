829ab5f797f5170a27eb4e2310d40e3e69bbdead
JuneChang <junezm@foxmail.com>
2026-01-13 17:24:27 +0800

    05: firecrawl ai

--- src/app/api/demo/blocking/route.ts
+++ src/app/api/demo/blocking/route.ts
@@ -1,11 +1,34 @@
 import { generateText } from "ai";
-import { google } from '@ai-sdk/google';
+import { google } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import OpenAI from "openai";
+
+// const openai = createOpenAI({
+//   // custom settings, e.g.
+//   baseURL: "https://api-inference.modelscope.cn/v1",
+//   apiKey: "ms-a4aee938-1f20-4bc1-8ba3-86eef1318320",
+// });
+
+const openai = new OpenAI({
+  // 若没有配置环境变量，请用百炼API Key将下行替换为：apiKey: "sk-xxx",
+  apiKey: process.env.DASHSCOPE_API_KEY,
+  baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1",
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "qwen-plus", //此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: "今天成都天气怎么样？" },
+    ],
+  });
+  console.log(JSON.stringify(completion));
+  return completion;
+}
 
 export async function POST() {
-  const response = await generateText({
-    model: google("gemini-2.5-flash"),
-    prompt: "Hello, how are you?",
-  });
+  const response = await main();
 
   return Response.json({ response });
 }
